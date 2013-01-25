c6de8b9a627cd5c24a6fc4cae9f036d09102e287
Yorick Terweijden <terwey@gmail.com>
2013-01-25 11:03:12 +0100

    Generate command works. pdflatex is called by default on generate with the format tex

--- src/php/fancycv/GenerateCommand.php
+++ src/php/fancycv/GenerateCommand.php
@@ -21,6 +21,33 @@ class GenerateCommand extends Command
                 InputOption::VALUE_NONE,
                 'Process your positions'
             )
+            ->addOption(
+                'format',
+                '-f',
+                InputOption::VALUE_REQUIRED,
+                'Specifies in which format it should be generated.',
+                'tex'
+            )
+            ->addOption(
+                'output',
+                '-o',
+                InputOption::VALUE_REQUIRED,
+                'Name of generated output.',
+                'generated'
+            )
+            ->addOption(
+                'directory',
+                '-d',
+                InputOption::VALUE_REQUIRED,
+                'Name of output directory.',
+                'output'
+            )
+            ->addOption(
+                'generateOnly',
+                '-g',
+                InputOption::VALUE_NONE,
+                'Generate only, disabled post-processor (e.g. pdflatex)'
+            )
         ;
     }
 
@@ -32,12 +59,37 @@ class GenerateCommand extends Command
             $returnCode = $command->run($input, $output);
         } else {
             $decode = json_decode(file_get_contents(JSON_FILE), TRUE);
-            $format = new Format('tex');
+            $format = new Format($input->getOption('format'));
             // print_r($decode['positions']['values']);
             $positionsTex = $this->positions($format, $decode['positions']['values']);
-            print $format->documentOpen();
-            print $positionsTex;
-            print $format->documentClose();
+            $file  = $format->documentOpen();
+            $file .= $positionsTex;
+            $file .= $format->documentClose();
+            $outputDir = BASE_DIR.'/'.$input->getOption('directory');
+            if (!file_exists($outputDir)) {
+                // attempt to create it
+                if (!mkdir($outputDir)) {
+                    $output->writeln('<error>The output dir does not exist and could not be created: '.$outputDir.'</error>');
+                    exit(0);
+                }
+            }
+            $fileLocation = $outputDir.'/'.$input->getOption('output').'.'.$input->getOption('format');
+            if (file_put_contents($fileLocation, $file)) {
+                $output->writeln('<info>File successfully generated: '.$fileLocation.'</info>');
+            } else {
+                $output->writeln('<error>Could not write file.</error>');
+            }
+            if (!$input->getOption('generateOnly') && $input->getOption('format') == 'tex') {
+                $execOutput = array();
+                $execReturn;
+                exec('pdflatex -halt-on-error -interaction errorstopmode -output-directory '.$outputDir.' '.$fileLocation, $execOutput, $execReturn);
+                if ($execReturn == 0) {
+                    $lines = count($execOutput);
+                    $output->writeln('<info>'.$execOutput[$lines-4].'</info>');
+                } else {
+                    $output->writeln('<error>Executing pdflatex failed. Output: '.implode("\n", $execOutput).'</error>');
+                }
+            }
         }
     }
 
